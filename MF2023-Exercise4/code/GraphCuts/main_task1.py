e839e6b02b7f19b3fadffe62d837e50666c4f673
oalkaya <omeralkaya1999@gmail.com>
2023-04-23 13:03:08 +0200

    task 2.1

--- MF2023-Exercise4/code/GraphCuts/main_task1.py
+++ MF2023-Exercise4/code/GraphCuts/main_task1.py
@@ -1,10 +1,41 @@
 from graph_cut import GraphCut
 import numpy as np
+from scipy.sparse import csr_matrix
 
 
 def main():
     # TODO: Implement Task 1
-    pass
+
+    # Nodes = # non-source/sink vertices
+    # Connections = # non-terminal edges
+    g = GraphCut(nodes=3, connections=4)
+
+    # Non-terminal edge capacities as adjacency matrix
+    edges = np.array([
+                [0, 1, 3, 2], # bidirectional edge between vertices 0-1
+                [1, 2, 5, 1], # bidirectional edge between vertices 1-2
+                ])
+
+    g.set_pairwise(edges)   
+
+    # Set the unaries
+    unaries = np.array([
+        [9, 4],  # vertex 1
+        [7, 7],  # vertex 2
+        [5, 8]   # vertex 3
+    ])
+
+    g.set_unary(unaries)
+
+    # Minimize, returns maxflow
+    print(g.minimize())
+
+    # Get labels: False if node belongs to source, True if node belongs to sink
+    print(g.get_labeling())
+
+
+    # We get maxflow = 19 for this given graph. Manual calculation also confirms that.
+    # A possible min cut is through the terminal edges between the vertices and sink.
 
 
 if __name__ == '__main__':
